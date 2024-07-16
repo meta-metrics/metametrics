ba74f19ded90009f23fb1c01338e4f08c8181698
Exqrch <luckyblock0@gmail.com>
2024-07-16 17:42:58 +0700

    Evouna dataset class implemented

---
+++ tasks/qaeval/evouna_dataset.py
@@ -0,0 +1,73 @@
+import json
+import os
+import pandas as pd
+
+class EVOUNA_Dataset:
+    """
+    A Class to load the EVOUNA dataset
+
+    The EVOUNA dataset consists of two subset: NQ ("Natural Questions") and TQ ("Trivia Questions")
+
+    You can load a dataset by performing:
+        tq = EVOUNA_Dataset("TQ")
+
+    The EVOUNA Dataset contains non-compliant entry that can be filtered.
+    If you want the original dataset that contains both improper and proper content, use the load_dataset() method:
+        tq = EVOUNA_Dataset("TQ")
+        tq_full_data = tq.load_dataset()
+    
+    To obtain only proper content, use process_dataset() prior to calling load_dataset():
+        tq = EVOUNA_Dataset("TQ")
+        tq.process_dataset()
+        tq_full_data = tq.load_dataset()
+    """
+
+    # Helper Methods
+    @staticmethod
+    def load_json(path):
+        with open(path) as f:
+            data = json.load(f)
+        return data
+    
+    @staticmethod
+    def split_by_data_improperness(lst):
+        improper_list = []
+        proper_list = []
+
+        for data_entry in lst:
+            if data_entry['improper']:
+                improper_list.append(data_entry)
+            else:
+                proper_list.append(data_entry)
+
+        return proper_list, improper_list
+    # End of Helper Methods
+
+    def __init__(self, dataset_name):
+        """
+        Initialize the QA_Eval class with a specific dataset.
+
+        Args:
+            dataset_name (str): Name of the dataset ("NQ", "TQ").
+        """
+        assert dataset_name in ["NQ", "TQ"], 'dataset_name must either be "NQ" or "TQ"'
+        if dataset_name == "NQ":
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            data_dir = os.path.join(script_dir, "data")
+            self.data = EVOUNA_Dataset.load_json(os.path.join(data_dir, "NQ.json"))
+        elif dataset_name == "TQ":
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            data_dir = os.path.join(script_dir, "data")
+            self.data = EVOUNA_Dataset.load_json(os.path.join(data_dir, "NQ.json"))
+
+    def load_dataset(self):
+        """
+        Load the specified dataset.
+        """
+        return pd.DataFrame(self.data)
+
+    def preprocess_dataset(self):
+        """
+        Preprocess the loaded dataset.
+        """
+        self.data, _ = EVOUNA_Dataset.split_by_data_improperness(self.data)
