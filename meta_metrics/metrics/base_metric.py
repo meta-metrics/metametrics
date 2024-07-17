ec52c1979e561f929ef6172f6647bf29a1a601a3
gentaiscool <gentaindrawinata@gmail.com>
2024-07-16 20:23:00 -0400

    update metricx

--- meta_metrics/metrics/base_metric.py
+++ meta_metrics/metrics/base_metric.py
@@ -1,5 +1,5 @@
 from typing import List, Tuple
 
 class BaseMetric:
-    def score(self, predictions:List[str], references:List[str]) -> List[float]:
+    def score(self, predictions:List[str], references:List[str], sources:List[str]=None) -> List[float]:
         raise NotImplementedError()
