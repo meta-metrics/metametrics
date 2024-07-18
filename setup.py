560a17c90f9e5a9acd3ec78c45f021d674b59d3b
David Anugraha <david.anugraha@gmail.com>
2024-07-18 18:35:57 -0400

    New setup automation

--- setup.py
+++ setup.py
@@ -1,4 +1,59 @@
+import subprocess
+import os
+import logging
 from setuptools import find_packages, setup
+from setuptools.command.install import install
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+
+# Define a setup command
+class SetupInstallCommand(install):
+    description = 'Run additional shell commands for setup'
+    user_options = []
+
+    def run(self):
+        # Navigate to the meta_metrics/metrics directory
+        os.chdir('meta_metrics/metrics')
+
+        # Clone BLEURT repository if it doesn't exist
+        if not os.path.isdir('bleurt'):
+            logging.info("Cloning BLEURT repository ...")
+            subprocess.run(["git", "clone", "https://github.com/google-research/bleurt.git"])
+            
+            # Navigate to the bleurt directory
+            os.chdir('bleurt')
+
+            # Install BLEURT
+            logging.info("Installing BLEURT ...")
+            result = subprocess.run(["pip", "install", "."], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if result.returncode == 0:
+                logging.info("BLEURT installed successfully.")
+            else:
+                logging.error("Failed to install BLEURT.")
+                logging.error(result.stderr.decode())
+                return
+        else:
+            logging.info("Skipping BLEURT installation as BLEURT already exists.")
+
+        # Clone YiSi repository if it doesn't exist
+        if not os.path.isdir('yisi'):
+            logging.info("Cloning YiSi repository ...")
+            subprocess.run(["git", "clone", "https://github.com/davidanugraha/yisi.git"])
+            os.chdir('yisi/src')
+            logging.info("Installing YiSi ...")
+            result = subprocess.run(["make", "all", "-j", "8"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if result.returncode == 0:
+                logging.info("YiSi installed successfully.")
+            else:
+                logging.error("Failed to install YiSi.")
+                logging.error(result.stderr.decode())
+                return
+        else:
+            logging.info("Skipping YiSi installation as YiSi already exists.")
+        
+        # Run the standard install process
+        install.run(self)
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -31,9 +86,9 @@ setup(
         "sacrebleu==2.4.2",
         "unbabel-comet==2.2.2",
         "pandas",
-        "numpy",
-        "tensorflow",
-        "torch",
+        "numpy==1.26.4",
+        "tensorflow==2.16.2",
+        "torch==2.3.1",
         "torchvision",
         "tf-slim>=1.1",
         "bert_score",
@@ -42,4 +97,7 @@ setup(
         "gdown"
     ],
     python_requires=">=3.10",
+    cmdclass={
+        'install': SetupInstallCommand,
+    },
 )
