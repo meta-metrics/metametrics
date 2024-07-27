42f577a73807fd18ace29c6aca1c100c55d5d2aa
Genta Winata <gentaindrawinata@gmail.com>
2024-07-27 23:38:59 +0000

    update name

--- tasks/wmt24/generate_score.py
+++ tasks/wmt24/generate_score.py
@@ -12,24 +12,24 @@ if __name__ == "__main__":
 
     metrics_configs = [
         ##### With Reference
-        # ("bertscore", {"model_name": "microsoft/deberta-xlarge-mnli", "model_metric": "precision"}),
-        # ("bertscore", {"model_name": "microsoft/deberta-xlarge-mnli", "model_metric": "recall"}),
-        # ("bertscore", {"model_name": "microsoft/deberta-xlarge-mnli", "model_metric": "f1"}),
+        # ("bertscore", {"model_name": "microsoft/deberta-xlarge-mnli", "model_metric": "precision"}, False),
+        # ("bertscore", {"model_name": "microsoft/deberta-xlarge-mnli", "model_metric": "recall"}, False),
+        # ("bertscore", {"model_name": "microsoft/deberta-xlarge-mnli", "model_metric": "f1"}, False),
         
-        # ("yisi", {"model_name": "xlm-roberta-base", "alpha": 0.8}),
-        # ("metricx", {"model_name": "google/metricx-23-xxl-v2p0", "batch_size": 1, 'is_qe': False, 'tokenizer_name': "google/mt5-xxl", 'max_input_length': 1024, "bf16": True}),
-        # ("metricx", {"model_name": "google/metricx-23-xl-v2p0", "batch_size": 1, 'is_qe': False, 'tokenizer_name': "google/mt5-xl", 'max_input_length': 1024, "bf16": True}),
-        # ("metricx", {"model_name": "google/metricx-23-large-v2p0", "batch_size": 1, 'is_qe': False, 'tokenizer_name': "google/mt5-large", 'max_input_length': 1024, "bf16": True}),
-        # ("comet", {"hf_token": "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", "batch_size": 1}),
-        ("xcomet-xl", {"hf_token": "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", "batch_size": 1}),
-        # ("xcomet-xxl", {"hf_token": "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", "batch_size": 1}),
+        # ("yisi", {"model_name": "xlm-roberta-base", "alpha": 0.8}, False),
+        # ("metricx", {"model_name": "google/metricx-23-xxl-v2p0", "batch_size": 1, 'is_qe': False, 'tokenizer_name': "google/mt5-xxl", 'max_input_length': 1024, "bf16": True}, False),
+        # ("metricx", {"model_name": "google/metricx-23-xl-v2p0", "batch_size": 1, 'is_qe': False, 'tokenizer_name': "google/mt5-xl", 'max_input_length': 1024, "bf16": True}, False),
+        # ("metricx", {"model_name": "google/metricx-23-large-v2p0", "batch_size": 1, 'is_qe': False, 'tokenizer_name': "google/mt5-large", 'max_input_length': 1024, "bf16": True}, False),
+        # ("comet", {"hf_token": "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", "batch_size": 1}, False),
+        ("xcomet-xl", {"hf_token": "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", "batch_size": 1}, False),
+        # ("xcomet-xxl", {"hf_token": "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", "batch_size": 1}, False),
 
         ##### Reference-Free
-        # ("metricx", {"model_name": "google/metricx-23-xxl-v2p0", "batch_size": 1, 'is_qe': True, 'tokenizer_name': "google/mt5-xxl", 'max_input_length': 1024, "bf16": True}),
-        # ("metricx", {"model_name": "google/metricx-23-xl-v2p0", "batch_size": 1, 'is_qe': True, 'tokenizer_name': "google/mt5-xl", 'max_input_length': 1024, "bf16": True}),
-        # ("metricx", {"model_name": "google/metricx-23-large-v2p0", "batch_size": 1, 'is_qe': True, 'tokenizer_name': "google/mt5-large", 'max_input_length': 1024, "bf16": True}),        
-        # ("cometkiwi", {"hf_token": "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", "batch_size": 8}),
-        # ("cometkiwi-xxl", {"hf_token": "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", "batch_size": 1}),
+        # ("metricx", {"model_name": "google/metricx-23-xxl-v2p0", "batch_size": 1, 'is_qe': True, 'tokenizer_name': "google/mt5-xxl", 'max_input_length': 1024, "bf16": True}, True),
+        # ("metricx", {"model_name": "google/metricx-23-xl-v2p0", "batch_size": 1, 'is_qe': True, 'tokenizer_name': "google/mt5-xl", 'max_input_length': 1024, "bf16": True}, True),
+        # ("metricx", {"model_name": "google/metricx-23-large-v2p0", "batch_size": 1, 'is_qe': True, 'tokenizer_name': "google/mt5-large", 'max_input_length': 1024, "bf16": True}, True),        
+        # ("cometkiwi", {"hf_token": "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", "batch_size": 8}, True),
+        # ("cometkiwi-xxl", {"hf_token": "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", "batch_size": 1}, True),
     ]
     
     all_metric_names = "_".join(config[0] for config in metrics_configs)
@@ -59,6 +59,8 @@ if __name__ == "__main__":
                 
         for metric_id in range(len(metrics_configs)):
             metric_name = metrics_configs[metric_id][0]
+            if metrics_configs[metric_id][2]:
+                metric_name + "_reference_free"
             metric = MetaMetrics([metrics_configs[metric_id]], weights=[1])
             new_df[metric_name] = np.array(metric.score(predictions, references, sources))
 
