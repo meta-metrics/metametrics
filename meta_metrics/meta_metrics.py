701860ab6d5a7d05ae16b9e85a56217a96685f70
gentaiscool <gentaindrawinata@gmail.com>
2024-07-28 22:29:51 -0400

    add mem efficient code

--- meta_metrics/meta_metrics.py
+++ meta_metrics/meta_metrics.py
@@ -18,46 +18,61 @@ class MetaMetrics:
             metrics_configs (List[Tuple[str, dict]]): a list of tuple of metric with the metric name and arguments.
             weights (List[float]): a list of float weight assigned to each metric
     """
-    def __init__(self, metrics_configs:List[Tuple[str, dict]], weights:List[float] = None):
+    def __init__(self, metrics_configs:List[Tuple[str, dict]], weights:List[float] = None, mem_efficient=False):
         self.metrics_configs = metrics_configs
         self.metrics = []
         self.weights = weights
+        self.mem_efficient = mem_efficient
 
-        self.init_metrics()
-        assert len(self.metrics) == len(self.weights)
+        if not self.mem_efficient:
+            self.init_metrics()
+            assert len(self.metrics) == len(self.weights)
 
+    def get_metric(self, metric_name, metric_args):
+        metric = None
+        if metric_name == "bertscore":
+            metric = BERTScoreMetric(**metric_args)
+        elif metric_name == "bleurt":
+            metric = BLEURT20Metric()
+        elif metric_name == "comet":
+            metric = COMETMetric(comet_model="Unbabel/wmt22-comet-da", **metric_args)
+        elif metric_name == "xcomet-xxl":
+            metric = COMETMetric(comet_model="Unbabel/XCOMET-XXL", **metric_args)
+        elif metric_name == "xcomet-xl":
+            metric = COMETMetric(comet_model="Unbabel/XCOMET-XL", **metric_args)
+        elif metric_name == "cometkiwi":
+            metric = COMETMetric(comet_model="Unbabel/wmt22-cometkiwi-da", **metric_args)
+        elif metric_name == "cometkiwi-xxl":
+            metric = COMETMetric(comet_model="Unbabel/wmt23-cometkiwi-da-xxl", **metric_args)
+        elif metric_name == "metricx":
+            metric = MetricXMetric(**metric_args)
+        elif metric_name == "yisi":
+            metric = YiSiMetric(**metric_args)
+        elif metric_name =="gemba_mqm":
+            metric = GEMBA_MQM(**metric_args)
+        return metric
+    
     def init_metrics(self):
         for i in range(len(self.metrics_configs)):
             metric_config = self.metrics_configs[i]
             metric_name = metric_config[0]
             metric_args = metric_config[1]
-
-            if metric_name == "bertscore":
-                metric = BERTScoreMetric(**metric_args)
-            elif metric_name == "bleurt":
-                metric = BLEURT20Metric()
-            elif metric_name == "comet":
-                metric = COMETMetric(comet_model="Unbabel/wmt22-comet-da", **metric_args)
-            elif metric_name == "xcomet-xxl":
-                metric = COMETMetric(comet_model="Unbabel/XCOMET-XXL", **metric_args)
-            elif metric_name == "xcomet-xl":
-                metric = COMETMetric(comet_model="Unbabel/XCOMET-XL", **metric_args)
-            elif metric_name == "cometkiwi":
-                metric = COMETMetric(comet_model="Unbabel/wmt22-cometkiwi-da", **metric_args)
-            elif metric_name == "cometkiwi-xxl":
-                metric = COMETMetric(comet_model="Unbabel/wmt23-cometkiwi-da-xxl", **metric_args)
-            elif metric_name == "metricx":
-                metric = MetricXMetric(**metric_args)
-            elif metric_name == "yisi":
-                metric = YiSiMetric(**metric_args)
-            elif metric_name =="gemba_mqm":
-                metric = GEMBA_MQM(**metric_args)
+            metric = self.get_metric(metric_name, metric_args)
             self.metrics.append(metric)
 
     def score(self, predictions:List[str], references:List[str], sources: List[str] = None) -> List[float]:
         overall_metric_score = None
         for i in range(len(self.metrics)):
-            metric_score = np.array(self.metrics[i].score(predictions, references, sources))
+            if self.mem_efficient:
+                metric_config = self.metrics_configs[i]
+                metric_name = metric_config[0]
+                metric_args = metric_config[1]
+                metric = self.get_metric(metric_name, metric_args)
+                metric_score = np.array(metric.score(predictions, references, sources))
+                del metric # for efficiency
+            else:
+                metric_score = np.array(self.metrics[i].score(predictions, references, sources))
+            
             if i == 0:
                 overall_metric_score = metric_score * self.weights[i]
             else:
