33683dd768f5e6d314d2fa8c8bbf9f00cbfe5b31
Exqrch <luckyblock0@gmail.com>
2024-07-29 15:31:55 +0700

    Added runable script and batch generate

---
+++ tasks/wmt24/generate_score_xxl_batch.py
@@ -0,0 +1,79 @@
+import os
+
+import pandas as pd
+import numpy as np
+from datasets import load_dataset
+
+from meta_metrics import MetaMetrics
+
+import GPUtil
+from threading import Thread
+import time
+
+class Monitor(Thread):
+    def __init__(self, delay):
+        super(Monitor, self).__init__()
+        self.stopped = False
+        self.delay = delay # Time between calls to GPUtil
+        self.start()
+
+    def run(self):
+        while not self.stopped:
+            GPUtil.showUtilization()
+            time.sleep(self.delay)
+
+    def stop(self):
+        self.stopped = True
+    
+if __name__ == "__main__":
+    # Instantiate monitor with a 10-second delay between updates
+    monitor = Monitor(10)
+    
+    dataset_names =  ["wmt-sqm-human-evaluation", "wmt-mqm-human-evaluation", "wmt-da-human-evaluation"]
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+    metrics_configs = [
+        ("xcomet-xxl", {"hf_token": "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", "batch_size": 32}, False),
+        ("cometkiwi-xxl", {"hf_token": "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", "batch_size": 32}, True),
+    ]
+    
+    all_metric_names = "_".join(config[0] for config in metrics_configs)
+
+    for dataset_name in dataset_names:
+        # Saves locally as CSV, if exists just read from CSV
+        local_csv_path = os.path.join(cur_dir, f"output/{dataset_name}.csv")
+        if not os.path.exists(local_csv_path):
+            os.makedirs(os.path.join(cur_dir, "output"), exist_ok=True)
+            dataset = load_dataset(f"RicardoRei/{dataset_name}", split="train")
+            df = pd.DataFrame(dataset)
+
+            # Drop rows where any of the specified columns have NaN values
+            df = df.dropna(subset=['src', 'mt', 'ref'])
+            df['id'] = range(0, len(df))
+            df.to_csv(local_csv_path, sep='|', index=False)
+        else:    
+            df = pd.read_csv(local_csv_path, sep='|')
+        
+        predictions = df["mt"].to_list()
+        references = df["ref"].to_list()
+        sources = df["src"].to_list()
+        print(len(predictions))
+        
+        new_df = pd.DataFrame()
+        new_df['id'] = df['id']
+                
+        for metric_id in range(len(metrics_configs)):
+            metric_name = metrics_configs[metric_id][0]
+            if metrics_configs[metric_id][2]:
+                metric_name += "_reference_free"
+            if metric_name == "bertscore":
+                model_metric = metrics_configs[metric_id][1]["model_metric"]
+                metric_name += f"_{model_metric}"
+            
+            metric = MetaMetrics([metrics_configs[metric_id]], weights=[1])
+            new_df[metric_name] = np.array(metric.score(predictions, references, sources))
+
+        new_df.to_csv(os.path.join(cur_dir, f"output/{dataset_name}_with_{all_metric_names}.csv"), index=False)
+        
+    # Close monitor
+    monitor.stop()
