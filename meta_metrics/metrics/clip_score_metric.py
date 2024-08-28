cf3c5354052c1af13d86212471c90b30792ec25c
gentaiscool <gentaindrawinata@gmail.com>
2024-08-27 20:22:44 -0400

    update clipscore code

--- meta_metrics/metrics/clip_score_metric.py
+++ meta_metrics/metrics/clip_score_metric.py
@@ -1,28 +1,15 @@
-from torchmetrics.functional.multimodal import clip_score
-from functools import partial
-
 import torch
-import os
-import logging
-import requests
 import clip
-from zipfile import ZipFile
 from typing import List, Union
 from tqdm import tqdm
 
-import argparse
 from PIL import Image
-from sklearn.preprocessing import normalize
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
 import torch
 import tqdm
 import numpy as np
 import sklearn.preprocessing
 import collections
-import pathlib
-import json
-import generation_eval_utils
-import pprint
 import warnings
 
 from packaging import version
@@ -100,9 +87,9 @@ class ClipScoreMetric(VisionToTextBaseMetric):
     """
 
     """
-    def __init__(self, is_reference_free:bool, model_name: str, device: str, w: float=2.5, **kwargs):
+    def __init__(self, is_reference_only:bool, model_name: str, device: str, w: float=2.5, **kwargs):
         # self.clip_score_fn = partial(clip_score, model_name_or_path=model_name)
-        self.is_reference_free = is_reference_free
+        self.is_reference_only = is_reference_only
         self.model_name = model_name
 
         self.model, self.transform = clip.load("ViT-B/32", device=device, jit=False)
@@ -112,63 +99,56 @@ class ClipScoreMetric(VisionToTextBaseMetric):
         self.w = w
 
 
-    def score(self, image_sources: List[torch.Tensor], text_predictions: List[str], text_references: Union[None, List[str]]=None, text_sources: Union[None, List[str]]=None) -> List[float]:
-        # prompts = []
-        # for i in range(len(text_predictions)):
-        #     prompts.append(text_predictions[i])
-        
-        # images_int = (image_sources * 255).astype("uint8")
-        # clip_score = self.clip_score_fn(torch.from_numpy(images_int).permute(0, 3, 1, 2), prompts).detach()
-        # return round(float(clip_score), 4)
-        if self.is_reference_free:
-            if isinstance(candidates, list):
-                candidates = extract_all_captions(candidates, self.model, self.device)
+    def score(self, image_sources: List[str], text_predictions: List[str], text_references: Union[None, List[str]]=None, text_sources: Union[None, List[str]]=None) -> List[float]:
+        if self.is_reference_only:
+            if isinstance(text_predictions, list):
+                text_predictions = extract_all_captions(text_predictions, self.model, self.device)
 
             flattened_refs = []
             flattened_refs_idxs = []
-            for idx, refs in enumerate(references):
+            for idx, refs in enumerate(text_references):
                 flattened_refs.extend(refs)
                 flattened_refs_idxs.extend([idx for _ in refs])
 
             flattened_refs = extract_all_captions(flattened_refs, self.model, self.device)
 
             if version.parse(np.__version__) < version.parse('1.21'):
-                candidates = sklearn.preprocessing.normalize(candidates, axis=1)
+                text_predictions = sklearn.preprocessing.normalize(text_predictions, axis=1)
                 flattened_refs = sklearn.preprocessing.normalize(flattened_refs, axis=1)
             else:
                 warnings.warn(
                     'due to a numerical instability, new numpy normalization is slightly different than paper results. '
                     'to exactly replicate paper results, please use numpy version less than 1.21, e.g., 1.20.3.')
 
-                candidates = candidates / np.sqrt(np.sum(candidates**2, axis=1, keepdims=True))
+                text_predictions = text_predictions / np.sqrt(np.sum(text_predictions**2, axis=1, keepdims=True))
                 flattened_refs = flattened_refs / np.sqrt(np.sum(flattened_refs**2, axis=1, keepdims=True))
 
             cand_idx2refs = collections.defaultdict(list)
             for ref_feats, cand_idx in zip(flattened_refs, flattened_refs_idxs):
                 cand_idx2refs[cand_idx].append(ref_feats)
 
-            assert len(cand_idx2refs) == len(candidates)
+            assert len(cand_idx2refs) == len(text_predictions)
 
             cand_idx2refs = {k: np.vstack(v) for k, v in cand_idx2refs.items()}
 
             per = []
-            for c_idx, cand in tqdm.tqdm(enumerate(candidates)):
+            for c_idx, cand in tqdm.tqdm(enumerate(text_predictions)):
                 cur_refs = cand_idx2refs[c_idx]
                 all_sims = cand.dot(cur_refs.transpose())
                 per.append(np.max(all_sims))
 
-            return np.mean(per), per
+            return per
         else:
-            if isinstance(images, list):
+            if isinstance(image_sources, list):
                 # need to extract image features
-                images = extract_all_images(images, self.model, self.device)
+                images = extract_all_images(image_sources, self.model, self.device)
 
-                candidates = extract_all_captions(candidates, self.model, self.device)
+                text_predictions = extract_all_captions(text_predictions, self.model, self.device)
 
                 #as of numpy 1.21, normalize doesn't work properly for float16
                 if version.parse(np.__version__) < version.parse('1.21'):
                     images = sklearn.preprocessing.normalize(images, axis=1)
-                    candidates = sklearn.preprocessing.normalize(candidates, axis=1)
+                    text_predictions = sklearn.preprocessing.normalize(text_predictions, axis=1)
                 else:
                     warnings.warn(
                         'due to a numerical instability, new numpy normalization is slightly different than paper results. '
@@ -177,6 +157,6 @@ class ClipScoreMetric(VisionToTextBaseMetric):
                     candidates = candidates / np.sqrt(np.sum(candidates**2, axis=1, keepdims=True))
 
                 per = self.w*np.clip(np.sum(images * candidates, axis=1), 0, None)
-                return np.mean(per), per, candidates
+                return per
 
 
